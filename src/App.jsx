83dda17ea06a7e1a2d6fb3e927ea4fd155b9adab
ndkhoa1195 <ndkhoa1195@gmail.com>
2024-08-16 07:50:50 +0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ function App() {
         <div className="mx-6 my-16 flex flex-col items-center gap-16 text-white lg:mx-4 lg:my-0 lg:flex-row lg:gap-[45px]">
           {/* left content */}
           <div className="max-w-[525px] text-center lg:text-left">
-            <h1 className="mb-4 text-[28px] font-bold leading-9 tracking-[-0.292px] lg:mb-3 lg:mb-7 lg:text-[50px] lg:leading-[55px] lg:tracking-[-0.521px]">
+            <h1 className="mb-4 text-[28px] font-bold leading-9 tracking-[-0.292px] lg:mb-7 lg:text-[50px] lg:leading-[55px] lg:tracking-[-0.521px]">
               Learn to code by watching others
             </h1>
             <p className="font-medium leading-[26px]">
